c991f2f9069f6605987770fdcd73451c19dbd6ad
blelump <blelump@martex.nom.pl>
2022-02-18 19:38:50 +0100

    Add examples

---
+++ examples/node.js/index.js
@@ -0,0 +1,16 @@
+const { OcaJs, resolveFromZip } = require('oca.js-form-core');
+const { renderOCAForm } = require( 'oca.js-form-html');
+const fs = require('fs');
+
+(async function() {
+  let file = fs.readFileSync("./swiss_passport.zip");
+  const oca = await resolveFromZip(file);
+  const captureBaseSAI = oca.overlays[0].capture_base
+  const ocaJs = new OcaJs({})
+  const structure = await ocaJs.createStructure(oca)
+  console.log(structure);
+
+  let output = renderOCAForm(structure);
+  console.log(output);
+
+}());
